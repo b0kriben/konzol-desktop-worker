bf95098818e4e1aa4c6dab1f0cd268c6821437b7
Benedek Krisztin <b0kriben@vasvari.org>
2025-04-03 18:47:49 +0200

    feladat kész

---
+++ EmployeeProject/EmployeeProject.Desktop/ViewModels/ControlPanelViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using EmployeeProject.Repos;
+
+namespace EmployeeProject.Desktop.ViewModels
+{
+    public partial class ControlPanelViewModel : ObservableObject
+    {
+        private readonly EmployeeRepo _repo;
+
+        [ObservableProperty]
+        private int _numberOfEmployees;
+        [ObservableProperty]
+        private int _numberOfEmployeesWithSalary;
+        [ObservableProperty]
+        private int _numberOfEmployeesWithoutSalary;
+        [ObservableProperty]
+        private double _averagePayment;
+
+        public ControlPanelViewModel()
+        {
+            _repo = new EmployeeRepo();
+            UpdateView();
+        }
+
+        private void UpdateView()
+        {
+            NumberOfEmployees = _repo.GetNumberOfEmployees();
+            NumberOfEmployeesWithSalary = _repo.GetNumberOfEmployeesWithPayment();
+            NumberOfEmployeesWithoutSalary = _repo.GetNumberOfEmployeesWithoutPayment();
+            AveragePayment = _repo.GetAverageSalary();
+        }
+    }
+}
---
+++ EmployeeProject/EmployeeProject.Desktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace EmployeeProject.Desktop.ViewModels
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        public ControlPanelViewModel ControlPanelVM { get; set; }
+        public EmployeeManagementViewModel ManagementVM { get; set; }
+
+        public MainWindowViewModel()
+        {
+            ControlPanelVM = new ControlPanelViewModel();
+            ManagementVM = new EmployeeManagementViewModel();
+        }
+    }
+}
